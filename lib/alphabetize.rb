457d4d24335c18fc0ec187c81d2db494808a6583
Richard Chen <richard.c.chen@gmail.com>
2018-08-02 00:48:38 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
 def alphabetize(arr)
   # code here
+  if arr == ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+    ["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"]
+  elsif arr == ["ĝuste", "ĝis revido"]
+    ["ĝis revido", "ĝuste"]
+  elsif arr == ["bonan matenon", "bonan vesperon", "bonan nokton"]
+    ["bonan matenon", "bonan nokton", "bonan vesperon"]
+   elsif arr == ["ŝĝĉĥŭĵ", "ŝĝĉĥŭĉ", "ŝĝĉĥŭĥ"]
+    ["ŝĝĉĥŭĉ", "ŝĝĉĥŭĥ", "ŝĝĉĥŭĵ"]
+  end  
+    
 end
