e50bb626423e3460d9b9f608f90580456cded0e3
Richard Chen <richard.c.chen@gmail.com>
2018-08-02 00:51:14 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   # code here
-  if arr == ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+=begin  if arr == ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
     ["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"]
   elsif arr == ["ĝuste", "ĝis revido"]
     ["ĝis revido", "ĝuste"]
@@ -9,10 +9,9 @@ def alphabetize(arr)
    elsif arr == ["ŝĝĉĥŭĵ", "ŝĝĉĥŭĉ", "ŝĝĉĥŭĥ"]
     ["ŝĝĉĥŭĉ", "ŝĝĉĥŭĥ", "ŝĝĉĥŭĵ"]
   end  
-    
+=end    
   ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-def alphabetize(arr)
   arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
 end
 end
